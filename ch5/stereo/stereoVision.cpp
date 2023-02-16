a3b63ea02bf8c5513eb80c71600c30845bd07182
sj_songdo <kimsuji@inha.edu>
2023-02-16 20:26:06 +0900

    stereo

--- ch5/stereo/stereoVision.cpp
+++ ch5/stereo/stereoVision.cpp
@@ -8,63 +8,59 @@
 using namespace std;
 using namespace Eigen;
 
-// 文件路径
-string left_file = "./left.png";
-string right_file = "./right.png";
+string left_file="/home/sj/slambook2/studySLAM/ch5/stereo/left.png";
+string right_file="/home/sj/slambook2/studySLAM/ch5/stereo/right.png";
 
-// 在pangolin中画图，已写好，无需调整
-void showPointCloud(
-    const vector<Vector4d, Eigen::aligned_allocator<Vector4d>> &pointcloud);
+void showPointCloud(const vector<Vector4d, Eigen::aligned_allocator<Vector4d>> &pointcloud);
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv){
+    
+    // intrinsics
+    double fx=718.856, fy=718.856, cx=607.1928, cy=185.2157;
+    // baseline
+    double b=0.573;
 
-    // 内参
-    double fx = 718.856, fy = 718.856, cx = 607.1928, cy = 185.2157;
-    // 基线
-    double b = 0.573;
-
-    // 读取图像
     cv::Mat left = cv::imread(left_file, 0);
     cv::Mat right = cv::imread(right_file, 0);
-    cv::Ptr<cv::StereoSGBM> sgbm = cv::StereoSGBM::create(
-        0, 96, 9, 8 * 9 * 9, 32 * 9 * 9, 1, 63, 10, 100, 32);    // 神奇的参数
+    cv::Ptr<cv::StereoSGBM> sgbm=cv::StereoSGBM::create(  0, 96, 9, 8 * 9 * 9, 32 * 9 * 9, 1, 63, 10, 100, 32  );
+    // sgbm (making disparity map) is sensitive to parameters
     cv::Mat disparity_sgbm, disparity;
     sgbm->compute(left, right, disparity_sgbm);
-    disparity_sgbm.convertTo(disparity, CV_32F, 1.0 / 16.0f);
+    disparity_sgbm.convertTo(disparity, CV_32F, 1.0/16.0f); //disparity map?? d=uL-uR
 
-    // 生成点云
+    // compute the pointcloud
     vector<Vector4d, Eigen::aligned_allocator<Vector4d>> pointcloud;
 
-    // 如果你的机器慢，请把后面的v++和u++改成v+=2, u+=2
-    for (int v = 0; v < left.rows; v++)
-        for (int u = 0; u < left.cols; u++) {
-            if (disparity.at<float>(v, u) <= 0.0 || disparity.at<float>(v, u) >= 96.0) continue;
+    // change v++ and u++ to v+=2, u+=2 if your machine is slow. To get a sparser cloud.
+    for (int v=0; v<left.rows; v++){
+        for (int u=0; u<left.cols; u++){
+            if (disparity.at<float>(v,u)<=0.0 || disparity.at<float>(v,u)>=96.0) continue;
 
-            Vector4d point(0, 0, 0, left.at<uchar>(v, u) / 255.0); // 前三维为xyz,第四维为颜色
+            Vector4d point(0,0,0, left.at<uchar>(v,u)/255.0); //the first three are x,y,z, 4th is color
 
-            // 根据双目模型计算 point 的位置
-            double x = (u - cx) / fx;
-            double y = (v - cy) / fy;
-            double depth = fx * b / (disparity.at<float>(v, u));
-            point[0] = x * depth;
-            point[1] = y * depth;
-            point[2] = depth;
+            // compute the depth from disparity
+            double x=(u-cx)/fx;
+            double y=(v-cy)/fy;        //depth map aligned to left image coord
+            double depth=fx*b/(disparity.at<float>(v,u));
+            point[0]=x*depth;     //x is left normalized coord before origin update
+            point[1]=y*depth;
+            point[2]=depth;
 
             pointcloud.push_back(point);
         }
-
-    cv::imshow("disparity", disparity / 96.0);
+    }
+    cv::imshow("disparity", disparity/96.0);
     cv::waitKey(0);
-    // 画出点云
+
     showPointCloud(pointcloud);
     return 0;
 }
 
-void showPointCloud(const vector<Vector4d, Eigen::aligned_allocator<Vector4d>> &pointcloud) {
-
-    if (pointcloud.empty()) {
-        cerr << "Point cloud is empty!" << endl;
-        return;
+//Pangolin
+void showPointCloud(const vector<Vector4d, Eigen::aligned_allocator<Vector4d>> &pointcloud){
+    if(pointcloud.empty()){
+        cerr<<"Point cloud is empty()"<<endl;
+        return ;
     }
 
     pangolin::CreateWindowAndBind("Point Cloud Viewer", 1024, 768);
@@ -78,24 +74,25 @@ void showPointCloud(const vector<Vector4d, Eigen::aligned_allocator<Vector4d>> &
     );
 
     pangolin::View &d_cam = pangolin::CreateDisplay()
-        .SetBounds(0.0, 1.0, pangolin::Attach::Pix(175), 1.0, -1024.0f / 768.0f)
+        .SetBounds(0.0, 1.0, pangolin::Attach::Pix(175), 1.0, -1024.0f/768.0f)
         .SetHandler(new pangolin::Handler3D(s_cam));
 
-    while (pangolin::ShouldQuit() == false) {
+    while(pangolin::ShouldQuit()==false){
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
         d_cam.Activate(s_cam);
         glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 
         glPointSize(2);
         glBegin(GL_POINTS);
-        for (auto &p: pointcloud) {
+        for(auto &p: pointcloud){
             glColor3f(p[3], p[3], p[3]);
-            glVertex3d(p[0], p[1], p[2]);
+            glVertex3d(p[0], p[1],p[2]);
         }
         glEnd();
         pangolin::FinishFrame();
-        usleep(5000);   // sleep 5 ms
+        usleep(5000);
     }
+
+
     return;
 }
